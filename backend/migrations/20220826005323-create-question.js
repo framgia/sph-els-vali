29c05ba2db5cf3cb22f392ee46d226aa86b5dc7a
Vali-R <ruziboev.vali@sun-asterisk.com>
2022-08-26 11:40:57 +0500

    [BE] Create API to Signup users

---
+++ backend/migrations/20220826005323-create-question.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Questions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      choice_1: {
+        type: Sequelize.STRING
+      },
+      choice_2: {
+        type: Sequelize.STRING
+      },
+      choice_3: {
+        type: Sequelize.STRING
+      },
+      correct_answer: {
+        type: Sequelize.STRING
+      },
+      quiz_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Questions');
+  }
+};
---
+++ backend/migrations/20220826005512-create-user-answer.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('UserAnswers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      question_id: {
+        type: Sequelize.INTEGER
+      },
+      user_answer: {
+        type: Sequelize.STRING
+      },
+      user_lesson_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('UserAnswers');
+  }
+};

19077ca8346cc70c99b78aec92d4867af792d147
Vali <110364690+Vali-R@users.noreply.github.com>
2022-08-30 14:34:09 +0500

    [BE] Create API to Signup users (#3)
    * [BE] Create API to Signup users
    
    * added new lines, fixed env.example and added validation for first name and last name
    
    * added new lines
    
    * changed password type to string

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,21 @@
-require('dotenv').config()
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
+const db = require("./models/index");
+const userAuthRouter = require("./routes/userAuth");
 
-const app = express()
+const app = express();
 
-app.use('/', (req, res, next)=>{
-    res.send('My Express App!!!')
-})
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.listen(process.env.PORT)
+app.use(userAuthRouter);
+
+// check if connection with database is successful
+db.sequelize
+  .authenticate()
+  .then(() => {
+    app.listen(process.env.PORT);
+  })
+  .catch(() => {
+    throw Error("Unable to connect database");
+  });

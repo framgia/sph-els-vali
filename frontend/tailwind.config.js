a234813f09cb8870d48f4b7e94d16b21e84a3232
Vali-R <ruziboev.vali@sun-asterisk.com>
2022-08-31 11:41:33 +0500

    [FE] Create a route and page for signup

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "rgb(17, 26, 44)",
+        main_white:'#E4EAF2',
+        second_yelow: "#EAE55D",
+      },
+      screens: {
+        sm: "270px",
+      },
+    },
   },
   plugins: [],
-}
+};

e5d2e8e51b9e1d9b0b60011ce4af6d9b61cc9b3c
Vali <110364690+Vali-R@users.noreply.github.com>
2022-09-02 07:48:14 +0500

    [BE] Create API to login users (#10)
    * [BE] Create API to login users
    
    * validation bug fix

--- backend/controllers/userAuthController.js
+++ backend/controllers/userAuthController.js
@@ -34,6 +34,7 @@ const sendEmailConfirmation = (userId, email) => {
   });
 };
 
+//SIGNUP
 const signup = async (req, res, next) => {
   const { first_name, last_name, email, password } = req.body;
   const errors = validationResult(req);
@@ -56,8 +57,7 @@ const signup = async (req, res, next) => {
 
     await sendEmailConfirmation(user.id, email);
 
-    // For now I'm sending back user to see if everything went through, but later I"ll send email and token
-    res.status(200).json({ user });
+    res.status(200).json({ message: "User created" });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
@@ -87,4 +87,35 @@ const resendConfirmation = async (req, res) => {
   }
 };
 
-module.exports = { signup, confirmEmail, resendConfirmation };
+// LOGIN
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ error: errors.array()[0].msg });
+  }
+
+  try {
+    const user = await User.findOne({ where: { email } });
+    const doMatch = await bcrypt.compare(password, user.password);
+
+    if (!doMatch) {
+      return res.status(400).json({ error: "Incorrect email or password" });
+    }
+
+    if (!user.verified) {
+      return res
+        .status(401)
+        .json({ error: "You are not verified", verified: user.verified });
+    }
+    const token = createJWTToken(user.id);
+    res.status(200).json({ id: user.id, token, verified: user.verified });
+  } catch (err) {
+    res.status(400).json({
+      error: "Something went wrong, please try again later",
+      verified: true,
+    });
+  }
+};
+
+module.exports = { signup, confirmEmail, resendConfirmation, login };
--- backend/routes/userAuth.js
+++ backend/routes/userAuth.js
@@ -4,6 +4,7 @@ const {
   signup,
   confirmEmail,
   resendConfirmation,
+  login,
 } = require("../controllers/userAuthController");
 const { User } = require("../models");
 
@@ -29,9 +30,8 @@ router.post(
       "password",
       "Password should be at least 5 character long, please also make sure there is no white spaces"
     )
-      .trim()
       .isLength({ min: 5 })
-      .isAlphanumeric(),
+      .trim(),
     body("first_name", "Please enter valid first name")
       .trim()
       .isLength({ min: 1 }),
@@ -45,5 +45,14 @@ router.post(
 router.get("/signup/confirmation/:token", confirmEmail);
 
 router.post("/signup/resendconfirmation", resendConfirmation);
+
+router.post(
+  "/login",
+  [
+    body("email", "Incorrect email or password").isEmail().normalizeEmail(),
+    body("password", "Incorrect email or password").isLength({ min: 5 }).trim(),
+  ],
+  login
+);
 
 module.exports = router;

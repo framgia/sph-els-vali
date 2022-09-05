f8cb0a823ba409a7519ecc5c76a984035912e0dc
Vali <110364690+Vali-R@users.noreply.github.com>
2022-09-05 14:13:07 +0500

    [BE] Create an api for sending all user info (#14)
    * [BE] Create an api for sending all user info
    
    * remove mapping

--- backend/controllers/feedController.js
+++ backend/controllers/feedController.js
@@ -159,4 +159,22 @@ const getUserInfo = async (req, res, next) => {
   }
 };
 
-module.exports = { getActivity, getLearnigsCount, getUserInfo };
+const getAllUsersInfo = async (req, res, next) => {
+  try {
+    const users = await User.findAll({
+      attributes: ["id", "first_name", "last_name", "avatar_url"],
+    });
+    res.status(200).json({ users });
+  } catch (err) {
+    res
+      .status(400)
+      .json({ error: "Something went wrong, please try again later" });
+  }
+};
+
+module.exports = {
+  getActivity,
+  getLearnigsCount,
+  getUserInfo,
+  getAllUsersInfo,
+};

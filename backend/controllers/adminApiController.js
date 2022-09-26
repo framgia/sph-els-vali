d7a288b46c68dcd48308c92eff86ee1dbd15078c
Vali-R <ruziboev.vali@sun-asterisk.com>
2022-09-26 07:12:01 +0500

    [BE] Edit Categories

--- backend/controllers/adminApiController.js
+++ backend/controllers/adminApiController.js
@@ -1,3 +1,4 @@
+const { validationResult } = require("express-validator");
 const { Quiz } = require("../models");
 
 const deleteCategory = async (req, res, next) => {
@@ -19,4 +20,30 @@ const deleteCategory = async (req, res, next) => {
   }
 };
 
-module.exports = { deleteCategory };
+const editCategory = async (req, res, next) => {
+  const isAdmin = req.isAdmin;
+  const { id } = req.params;
+  const { name, description } = req.body;
+
+  const errors = validationResult(req);
+
+  try {
+    if (!isAdmin) {
+      return res.status(403).json({ error: "You are not an admin" });
+    }
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ error: errors.array()[0].msg });
+    }
+
+    await Quiz.update({ name, description }, { where: { id } });
+
+    res.status(200).json({ message: "Quiz category was successfully updated" });
+  } catch (err) {
+    res
+      .status(400)
+      .json({ error: "Something went wrong, please try again later" });
+  }
+};
+
+module.exports = { deleteCategory, editCategory };
--- backend/routes/adminApi.js
+++ backend/routes/adminApi.js
@@ -1,8 +1,21 @@
 const express = require("express");
-const { deleteCategory } = require("../controllers/adminApiController");
+const { body } = require("express-validator");
+const {
+  deleteCategory,
+  editCategory,
+} = require("../controllers/adminApiController");
 
 const router = express.Router();
 
 router.delete("/admin/categories/delete/:id", deleteCategory);
+
+router.put(
+  "/admin/categories/edit/:id",
+  [
+    body("name", "Invalid name").trim().isLength({ min: 1 }),
+    body("description", "Invalid description").trim().isLength({ min: 1 }),
+  ],
+  editCategory
+);
 
 module.exports = router;

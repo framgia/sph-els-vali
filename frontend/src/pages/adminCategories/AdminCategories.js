6dbbed5c75171557642eb3d69188450fe3ae8c7d
Vali-R <ruziboev.vali@sun-asterisk.com>
2022-09-27 14:17:30 +0500

    [BE/FE] Search Categories and order

--- frontend/src/pages/adminCategories/AdminCategories.js
+++ frontend/src/pages/adminCategories/AdminCategories.js
@@ -1,12 +1,32 @@
 import { useState } from "react";
 import useGetCategories from "../../hooks/useGetCategories";
+import usePagination from "../../hooks/usePagination";
 import AdminNavbar from "../components/AdminNavbar";
+import SearchBar from "../components/SearchBar";
+import SortItem from "../components/SortItem";
 import Category from "./components/Category";
 import HeaderSection from "./components/HeaderSection";
+import FooterSection from "../users/components/FooterSection";
 
 const AdminCategories = () => {
+  const [itemsPerPage, setItemsPerPage] = useState(5);
   const [forceUpdate, setForceupdate] = useState(false);
-  const { data } = useGetCategories(forceUpdate);
+  const [order, setOrder] = useState("");
+  const [search, setSearch] = useState("");
+  const { data } = useGetCategories(order, search, forceUpdate);
+
+  const { handlePageClick, currentItems, pageCount, setItemOffset } =
+    usePagination(itemsPerPage, data ? data : []);
+
+  const handleSearch = (e) => {
+    const handle = { selected: 0 };
+    setSearch(e);
+    handlePageClick(handle);
+
+    if (!e.length > 0) {
+      setItemOffset(0);
+    }
+  };
 
   return (
     <div className="min-h-[100vh] w-[100%] h-[100%] flex flex-col">
@@ -15,8 +35,13 @@ const AdminCategories = () => {
         <div className="bg-white p-2 rounded-md shadow-md">
           <HeaderSection />
 
-          {data &&
-            data.map(({ id, name, description }) => (
+          <div className="flex items-center space-x-3 p-2 justify-end border-b">
+            <SearchBar handleSearch={handleSearch} />
+            <SortItem setOrder={setOrder} />
+          </div>
+
+          {currentItems &&
+            currentItems.map(({ id, name, description }) => (
               <Category
                 key={id}
                 id={id}
@@ -30,6 +55,14 @@ const AdminCategories = () => {
               No categories yet
             </p>
           )}
+
+          <FooterSection
+            search={search}
+            handlePageClick={handlePageClick}
+            pageCount={pageCount}
+            setItemsPerPage={setItemsPerPage}
+            itemsPerPage={itemsPerPage}
+          />
         </div>
       </div>
     </div>
